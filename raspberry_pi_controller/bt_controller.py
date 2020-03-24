1be854983372cb98fef37bc6cceecf4fe3d84b74
Alexander <Alexander.Emanuelsson94@gmail.com>
2020-03-24 21:57:08 +0100

    Update bt_controller script with a class

--- raspberry_pi_controller/bt_controller.py
+++ raspberry_pi_controller/bt_controller.py
@@ -1,57 +1,76 @@
 import bluetooth
 import time
 
-# The bluetooth address for the door module
-bd_addr = "14:41:05:05:88:77"
+class BT_Controller:
 
-port = 1
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    def __init__(self, bd_address):
+        """"""
+        self.bd_addr = bd_address
+        self.port = 1
+        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.timeout = 20
 
-check_time = None
+        self.check_time = None
 
-try:
-    sock.connect((bd_addr, port))
-except:
-    raise("Could not connect to bt_addr: " + bd_addr)
+    def connect(self):
+        """"""
+        try:
+            self.sock.connect((self.bd_addr, port))
+            self.sock.settimeout(self.timeout)
+            print("Connected to HC-05 module, bd_addr: " + bd_addr)
+            return True
+        except:
+            print("Could not connect to bd_addr: " + bd_addr)
+            return False
 
-print("Connected to HC-05 module, bt address: " + bd_addr)
+    def start_arduino_listening_for_door_status(self):
+        """"""
+        self.sock.send("0x1")   # Send the connection event to the arduino
+        data = self.sock.recv(4).decode()
+        if not "OK" in data:
+            print("Arduino did not respond to 0x1...")
+            return False
+        else:
+            print("Arduino responded with OK...")
+            print("Arduino entered listening for door status...")
+            return True
 
-sock.settimeout(15.0)
-sock.send("0x1")    # Send the connection event to the arduino
-data = sock.recv(4).decode()
-if not "OK" in data:
-    print("NO OK FROM CONNECTION")
-    sock.close()
-    exit()
+    def run(self):
+        """"""
+        self.check_time = time.time()
 
-print("Start!")
+        while True:
+            data = self.sock.recv(3).decode()
+            print("Incoming: " + str(data))
 
-check_time = time.time()
+            if "0x2" in data:
+                self.sock.send("OK\r\n")
+                print("Outgoing: OK")
+            elif "0x3" in data:
+                self.sock.send("OK\r\n")
+                print("Outgoing: OK")
+            elif "0x4" in data:
+                self.sock.send("OK\r\n")
+                self.check_time = time.time()
+                print("Outgoing: OK")
 
-while True:
-    data = sock.recv(3).decode()
-    print("Incoming: " + str(data))
+            if (time.time() - self.check_time) > 20:
+                print("Time exceeded 20 seconds... exiting...")
+                return False
 
-    if "0x2" in data:
-        sock.send("OK\r\n")
-        print("Outgoing: OK")
-    elif "0x3" in data:
-        sock.send("OK\r\n")
-        print("Outgoing: OK")
-    elif "0x4" in data:
-        sock.send("OK\r\n")
-        check_time = time.time()
-        print("Outgoing: OK")
+    def close(self):
+        """"""
+        self.sock.close()
 
-    if (time.time() - check_time) > 20:
-        print("TIME EXCEEDED 20 SECONDS... EXITING")
-        sock.close()
+
+if __name__ == "__main__":
+    bd_addr = "14:41:05:05:88:77"       # The bluetooth address for the door module
+    bt_ctrl = BT_Controller(bd_addr)
+    if not bt_ctrl.connect():
         exit()
-
-
-
-sock.close()
-
-
-
-#if __name__ == "__main__":
+    if not bt_ctrl.start_arduino_listening_for_door_status():
+        bt_ctrl.close()
+        exit()
+    if not bt_ctrl.run()
+        bt_ctrl.close()
+        exit()
